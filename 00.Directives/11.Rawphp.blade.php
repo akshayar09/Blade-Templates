3feab083be32582bb4ebe22750a1f1fb67eabd6f
akshayar09 <akshayar0910@gmail.com>
2023-05-25 18:09:17 +0530

    Raw PHP code inside Blade

---
+++ 00.Directives/11.Rawphp.blade.php
@@ -0,0 +1,19 @@
+
+
+// To embed php code into the views.
+
+// Use @php directive.
+
+- example:-- 
+      @php
+         $counter = 1;
+      @endphp
+
+
+- example:--- 
+
+  // only to write a single PHP statement.
+
+  @php($counter=1)
+
+  
